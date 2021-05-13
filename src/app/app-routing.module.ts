cff0da212404da2c7225dd19f87386bc13a9d8eb
doroshv <vd@incredas.com>
2021-05-13 21:08:15 +0300

    - Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {FlickrImageComponent} from './flickr-image/flickr-image.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'flickr/images', pathMatch: 'full', component: FlickrImageComponent},
+  {path: '*', pathMatch: 'full', redirectTo: 'cloud/images'} // redirect all routes to the cloud images component
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ src/app/flickr-image/flickr-image.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-flickr-image',
+  templateUrl: './flickr-image.component.html',
+  styleUrls: ['./flickr-image.component.scss']
+})
+export class FlickrImageComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
