c3381e959b3d83f1569bb56fdd5e2154b22bb1ed
doroshv <vd@incredas.com>
2021-05-13 21:00:41 +0300

    - Added flickr-image component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlickrImageComponent } from './flickr-image/flickr-image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlickrImageComponent
   ],
   imports: [
     BrowserModule,
